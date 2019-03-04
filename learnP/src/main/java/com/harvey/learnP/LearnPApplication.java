7bb49be6a34223d074c3047bf17e4f0f0c06ec6b
fox <hui.han@cloudwiz.cn>
2019-03-04 20:58:52 +0800

    add project module learnP as a java learning point..

---
+++ learnP/src/main/java/com/harvey/learnP/LearnPApplication.java
@@ -0,0 +1,13 @@
+package com.harvey.learnP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnPApplication.class, args);
+	}
+
+}
